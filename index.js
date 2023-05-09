e07918b541b26b17caf1b2a026233ad08afc5893
pragathinarreddy <pragathinarreddy24gmail.com>
2023-05-09 09:40:29 +0530

    complete

---
+++ index.js
@@ -0,0 +1,27 @@
+let boxOne = document.querySelector('.first')
+let boxTwo = document.querySelector('.second')
+
+function generateRandomColor(){
+    let hexCharacters = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f']
+    let color = '#'
+    for(let i = 0 ; i < 6 ; i ++ ){
+        let randomNumber = Math.floor(Math.random() * 16);
+        color = color + hexCharacters[randomNumber] 
+    }
+    return color ;
+}
+function handleclick(){
+    let randomColor = generateRandomColor()
+    boxOne.style.backgroundColor = randomColor
+
+}
+function handleMouseMove(){
+    let randomColor = generateRandomColor()
+    boxTwo.style.backgroundColor = randomColor
+}
+
+boxOne.addEventListener('click' , handleclick)
+boxTwo.addEventListener('mousemove' , handleMouseMove)
+
+
+
